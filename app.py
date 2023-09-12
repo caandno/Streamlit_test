ef23ac164453bc04ad22a84b1cc8671979b704e5
caandno <casperhe@live.dk>
2023-09-12 15:08:06 +0200

    fungerene app

--- app.py
+++ app.py
@@ -3,25 +3,25 @@ from datetime import datetime, timedelta
 
 # Define the Streamlit app
 def main():
-    st.title("Bread Baking Time Calculator")
+    st.title(":bread: Brød beregneren :cookie:")
 
     # Get the user's input time
-    input_time = st.time_input("Enter a time:")
+    input_time = st.time_input("Vælg et start tidspunkt:")
 
     if input_time:
         input_datetime = datetime.combine(datetime.today(), input_time)
 
         # Calculate times with labels
         times = [
-            ("Surdej friskes opp", input_datetime + timedelta(hours=18)),
-            ("Dej samles", input_datetime + timedelta(hours=21)),
-            ("Sett i ovnen", input_datetime + timedelta(hours=22, minutes=20)),
-            ("Brød ferdig", input_datetime + timedelta(hours=22, minutes=20)),
+            ("Surdej friskes opp", input_datetime + timedelta(hours=0, minutes=0),input_datetime + timedelta(hours=0, minutes=0)),
+            ("Dej samles", input_datetime + timedelta(hours=18, minutes=0),input_datetime + timedelta(hours=22, minutes=0)),
+            ("Sett i ovnen", input_datetime + timedelta(hours=21, minutes=0), input_datetime + timedelta(hours=26, minutes=0)),
+            ("Brød ferdig", input_datetime + timedelta(hours=22, minutes=20), input_datetime + timedelta(hours=26, minutes=20)),
         ]
 
-        st.write("#### Times:")
-        for label, time in times:
-            st.write(f"- {label}: {time.strftime('%H:%M')}")
+        st.write("#### Tider:")
+        for label, time1, time2 in times:
+            st.write(f"- {label}: {time1.strftime('%H:%M')} - {time2.strftime('%H:%M')}")
 
 if __name__ == "__main__":
     main()
